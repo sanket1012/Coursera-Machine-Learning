998ad4783d9a7c2ed19c86e9a9741be8e3e5b1ac
Sanket Waghmare <40578235+sanket1012@users.noreply.github.com>
2018-07-19 09:36:57 -0500

    Add files via upload

---
+++ Excercise 7- K-means Clustering and Principal Component Analysis/computeCentroids.m
@@ -0,0 +1,44 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+for j=1:K
+    temp=zeros(1,n);
+    temp1=0;
+    for p=1:m
+        if (idx(p,1)==j)
+            temp=temp+X(p,:);
+            temp1=temp1+1;
+        end
+    end
+    sum(j,:)=temp;
+    centroids(j,:)=sum(j,:)/temp1;
+
+% =============================================================
+
+
+end
+
